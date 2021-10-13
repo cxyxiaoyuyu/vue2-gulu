db6960dc5dced2b05556a423954181bb08b0dead
cxyxiaoyuyu.com <923996381@qq.com>
2021-10-13 21:18:36 +0800

    toast autoClose

--- src/toast.vue
+++ src/toast.vue
@@ -6,6 +6,29 @@
 <script>
 export default {
   name: 'GuluToast',
+  props: {
+    autoClose: {
+      type: Boolean,
+      default: true
+    },
+    autoCloseDelay: {
+      type: Number,
+      default: 5
+    }
+  },
+  mounted(){
+    if(this.autoClose){
+      setTimeout(() => {
+        this.close()
+      }, this.autoCloseDelay * 1000);
+    }
+  },
+  methods: {
+    close(){
+      this.$el.remove()
+      this.$destroy
+    }
+  }
 }
 </script>
 <style scoped>

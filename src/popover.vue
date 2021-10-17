8c81412b2dddd66c9ac456cde8dffa82093addcb
cxyxiaoyuyu.com <923996381@qq.com>
2021-10-17 16:53:40 +0800

    popover support position props

--- src/popover.vue
+++ src/popover.vue
@@ -1,6 +1,6 @@
 <template>
  <div class="popover" @click="toggle">
-  <div class="content-wrapper" ref="contentWrapper" v-show="visible">
+  <div class="content-wrapper" ref="contentWrapper" v-show="visible" :class="{[`position-${position}`]:true}">
     <slot name="content"></slot>
   </div>
   <div class="triggerWrapper" ref="triggerWrapper">
@@ -15,12 +15,42 @@ export default {
   data(){
     return { visible: false}
   },
+  props: {
+    position: {
+      type: String,
+      default: 'top',
+      validator(value){
+        return ['top','bottom','left','right'].indexOf(value) >= 0
+      }
+    }
+  },
   methods: {
     positionContent(){
       document.body.appendChild(this.$refs.contentWrapper)
-      let {top,left} = this.$refs.triggerWrapper.getBoundingClientRect()
-      this.$refs.contentWrapper.style.left = left + window.scrollX +'px'
-      this.$refs.contentWrapper.style.top = top + window.scrollY + 'px'
+      let {top,left,height,width} = this.$refs.triggerWrapper.getBoundingClientRect()
+      if(this.position === 'top'){
+        this.$refs.contentWrapper.style.left = left + window.scrollX +'px'
+        this.$refs.contentWrapper.style.top = top + window.scrollY + 'px'
+      }else if(this.position === 'bottom'){
+        this.$refs.contentWrapper.style.left = left + window.scrollX +'px'
+        this.$refs.contentWrapper.style.top = top + window.scrollY + height + 'px'
+      }else if(this.position === 'left'){
+        // 与按钮居中对齐 向上移与按钮高度差的一般
+        this.$refs.contentWrapper.style.left = left + window.scrollX +'px'
+        // 不知道为啥这里的高度需要延时获取 ？？？
+        setTimeout(() => {
+          let contentHeight = this.$refs.contentWrapper.offsetHeight
+          this.$refs.contentWrapper.style.top = top + window.scrollY - (contentHeight - height) / 2 + 'px'
+        })
+      }else{
+        // 与按钮居中对齐 向上移与按钮高度差的一般
+        this.$refs.contentWrapper.style.left = left + width + window.scrollX +'px'
+        // 不知道为啥这里的高度需要延时获取 ？？？
+        setTimeout(() => {
+          let contentHeight = this.$refs.contentWrapper.offsetHeight
+          this.$refs.contentWrapper.style.top = top + window.scrollY - (contentHeight - height) / 2 + 'px'
+        })
+      }
     },
     clickDom(e){
       if(!this.$refs.contentWrapper.contains(e.target)){
@@ -62,12 +92,82 @@ export default {
   position: relative;
 }
 .content-wrapper {
-  border: 1px solid #ddd;
-  padding: 20px;
+  border: 1px solid #999;
+  padding: 8px 16px;
   background:white;
   border-radius: 5px;
   position: absolute;
-  transform: translateY(-100%); // 防止遮住trigger button
-  width: 300px;
+  max-width: 300px;
+  word-break: break-all;
+  filter: drop-shadow(0 0 3px #999);
+  ::before,::after {
+    width: 0;
+    height: 0;
+    content: '';
+    display: block;
+    border: 10px solid transparent;
+    left: 10px;
+    position: absolute;
+  }
+  &.position-top {
+    margin-top: -10px;
+    transform: translateY(-100%); // 防止遮住trigger button
+    ::before,::after {
+      left: 10px;
+    }
+    ::before {
+      border-top-color: #999;
+      top: 100%;
+    }
+    ::after {
+      border-top-color: white;
+      top: calc(100% - 1px);
+    }
+  }
+  &.position-bottom {
+    margin-top: 10px;
+    ::before,::after {
+      left: 10px;
+    }
+    ::before {
+      border-bottom-color: #999;
+      bottom: 100%;
+    }
+    ::after {
+      border-bottom-color: white;
+      bottom: calc(100% - 1px);
+    }
+  }
+  &.position-left {
+    transform: translateX(-100%); // 防止遮住trigger button
+    margin-left: -10px;
+    ::before,::after {
+      transform: translateY(-50%);
+      top: 50%;
+    }
+    ::before {
+      border-left-color: #999;
+      left: 100%;
+    }
+    ::after {
+      border-left-color: white;
+      left: calc(100% - 1px);
+    }
+  }  
+  &.position-right {
+    margin-left: 10px;
+    ::before,::after {
+      transform: translateY(-50%);
+      top: 50%;
+    }
+    ::before {
+      border-right-color: #999;
+      left: -20px;  // 不知道为啥right定位无效 ？？？
+    }
+    ::after {
+      border-right-color: white;
+      left: -19px;  // right 定位无效 ？？？
+    }
+  }  
 }
 </style>

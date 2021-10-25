7f69fb5a58108b4ce18f9168e4f7eb69d96eeaa4
cxyxiaoyuyu.com <923996381@qq.com>
2021-10-25 19:15:56 +0800

    导出所有组件

--- index.js
+++ index.js
@@ -2,5 +2,26 @@ import Button from './src/components/button'
 import ButtonGroup from './src/components/button-group'
 import Icon from './src/components/icon'
 import Input from './src/components/input'
+import Col from './src/components/col'
+import Row from './src/components/Row'
 
-export { Button,ButtonGroup,Icon,Input }
+import Layout from './src/components/layout'
+import Header from './src/components/header'
+import Sider from './src/components/sider'
+import Content from './src/components/content'
+import Footer from './src/components/footer'
+
+import Plugin from './src/components/plugin'
+
+import Tabs from './src/components/tabs'
+import TabsHead from './src/components/tabs-head'
+import TabsBody from './src/components/tabs-body'
+import TabsItem from './src/components/tabs-item'
+import TabsPane from './src/components/tabs-pane'
+
+import Popover from './src/components/popover'
+import Collapse from './src/components/collapse'
+import CollapseItem from './src/components/collapse-item'
+
+export { Button,ButtonGroup,Icon,Input,Col,Row,Layout,Header,Sider,Content,Footer,Plugin,
+        Tabs,TabsHead,TabsBody,TabsItem,TabsPane,Popover,CollapseItem,Collapse }
